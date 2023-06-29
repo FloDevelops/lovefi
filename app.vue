bd027d5c9a95b5866984ec1f8e444f330c7e8d15
FloDevelops <65234487+FloDevelops@users.noreply.github.com>
2023-06-28 22:25:01 -0400

    basic components

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+useHead({
+  title: 'LoveFi',
+  meta: [
+    { name: 'description', content: 'Financial Independance for you and your loved ones.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
+
+
 <template>
+    <theHeader />
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ components/theHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+
+    <div>
+      <div>
+        <nuxt-link to="/">LoveFi</nuxt-link> |
+        <nuxt-link to="/test">Test</nuxt-link>
+      </div>
+    </div>
+
+  </header>
+</template>
