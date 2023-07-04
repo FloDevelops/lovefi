dfbbe277949d50543b5395bc030504b284ff8ab3
FloDevelops <65234487+FloDevelops@users.noreply.github.com>
2023-07-03 23:09:27 -0400

    from Clerck starting to work

--- front_web/pages/login.vue
+++ front_web/pages/login.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+const { $clerk } = useNuxtApp()
+const showAuthLinks = ref(true)
+const userButton = ref<HTMLDivElement | null>(null)
+const user = useUser()
+
+watch(user, (value) => {
+  if (value) {
+    showAuthLinks.value = false
+
+    nextTick(() => {
+      $clerk.mountUserButton(userButton.value!)
+    })
+  } else {
+    showAuthLinks.value = true
+  }
+})
+</script>
+
 <template>
   <h1>Login page</h1>
 
-  <loginForm />
+  <div v-if="showAuthLinks">
+    <button @click="$clerk.openSignUp">Sign Up</button>
+    <button @click="$clerk.openSignIn">Sign In</button>
+  </div>
+
+  <div v-else ref="userButton" style="margin: auto;"></div>
 </template>
