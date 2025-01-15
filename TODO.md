a9b0f85d516ff4360e13fefc7b4e318d44ab36c8
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2025-01-15 23:23:59 +0100

    feat: style auth forms

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TO DO
+
+- [ ] Handle gracefully server error 500 when trying to register a new user with an already existing email
--- start/routes.ts
+++ start/routes.ts
@@ -25,7 +25,7 @@ router
     router.post('/register', [RegisterController, 'store']).as('register.store')
     router.get('/login', [LoginController, 'show']).as('login.show')
     router.post('/login', [LoginController, 'store']).as('login.store')
-    router.post('/logout', [LogoutController, 'handle']).as('logout')
+    router.post('/logout', [LogoutController, 'handle']).as('logout.handle')
   })
   .prefix('auth')
   .as('auth')
